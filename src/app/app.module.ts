95c1b4b96fb6722f13a9035ee1e863d493664684
yurysiruts <sirut.2003@gmail.com>
2021-09-12 23:32:22 +0200

    setting-e-commerce-app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroFormComponent } from './hero-form/hero-form.component';
+import { RootComponent } from './root/root.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroFormComponent
+    HeroFormComponent,
+    RootComponent,
+    ProductListComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+import { products } from '../shared/products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss']
+})
+export class ProductListComponent implements OnInit {
+
+  products = products;
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
